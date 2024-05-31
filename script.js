30b279b0b438153cc127ad05253e037e2514858c
Manacervela <120068817+Manacervela@users.noreply.github.com>
2024-05-30 22:35:18 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    }
+});
+
+var swiper = new Swiper(".mySwiper-3", {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        }
+    }
+});
+
+let tabInputs = document.querySelectorAll(".tabinput");
+
+tabInputs.forEach(function(input) {
+    input.addEventListener('click', function() {
+        let id = input.value;
+        let thisSwiper = document.getElementById('swiper' + id).swiper;
+        thisSwiper.update();
+    });
+});
